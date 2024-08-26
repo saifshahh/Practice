378bec6b87ac7d80352a46ab7a5b52cd1c6bdf9d
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-08-26 10:48:02 +0500

    Update Q1.cpp

--- DS/Lab-02/Q1.cpp
+++ DS/Lab-02/Q1.cpp
@@ -1,1 +1,49 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    int* jaggedArray[5];
+
+    for (int i = 0; i < 5; i++)
+    {
+        jaggedArray[i] = new int[5];
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cout<<"Enter the element: ";
+            int val;
+            cin>>val;
+            jaggedArray[i][j] = val;
+        }
+        
+    }
+    
+
+    for (int i = 0; i < 5; i++)
+    {
+        int* newarr = new int[10];
+
+        for (int j = 0; j < 5; j++)
+        {
+            newarr[j] = jaggedArray[i][j];
+        }
+
+        delete jaggedArray[i];
+
+        jaggedArray[i] = newarr;
+        
+    }
+
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            if (j >= 5) {
+                jaggedArray[i][j] = 0;
+            }
+            cout << jaggedArray[i][j] << " ";
+        }
+        cout << endl;
+    }  
+}
