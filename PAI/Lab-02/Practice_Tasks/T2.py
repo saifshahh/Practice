6b48eed362efee251759db67ebf513f12d3bfc8f
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-08-28 10:04:16 +0500

    Create T2.py

---
+++ PAI/Lab-02/Practice_Tasks/T2.py
@@ -0,0 +1,7 @@
+def calculate_area(length,width):
+    return length * width
+
+length = int(input('Enter the length: '))
+width = int(input('Enter the width: '))
+
+print(calculate_area(length,width))
