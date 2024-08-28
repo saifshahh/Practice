ad06c70b8b0bd71de05ac459cf53d5dab3ac4c1d
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-08-28 10:08:30 +0500

    Delete PAI/Lab-02/Q2.py

--- PAI/Lab-02/Q2.py
+++
@@ -1,6 +0,0 @@
-def checker(str):
-    if(str[-1]=='a' or str[-1]=='e' or str[-1]=='i' or str[-1]=='o' or str[-1]=='u'): print("Vowel")
-    else: print("Consonant")
-
-str = input("Enter the string: ")
-checker(str)
