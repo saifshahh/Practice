53579a8eee542e2459429daf78d2dc2ef58a5aa9
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-08-28 08:56:59 +0500

    Update Q2.py

--- PAI/Lab-02/Q2.py
+++ PAI/Lab-02/Q2.py
@@ -1,5 +1,6 @@
-str = input("Enter the string: ")
+def checker(str):
+    if(str[-1]=='a' or str[-1]=='e' or str[-1]=='i' or str[-1]=='o' or str[-1]=='u'): print("Vowel")
+    else: print("Consonant")
 
-if(str[-1]=='a' or str[-1]=='e' or str[-1]=='i' or str[-1]=='o' or str[-1]=='u'):
-    print("Vowel")
-else: print("Consonant")
+str = input("Enter the string: ")
+checker(str)
