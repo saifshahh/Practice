9c30295e061c25af9a4b4eecb65eac41655c5672
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-09-01 10:00:26 +0500

    Delete PAI/Lab-02/Lab_Tasks/Q9.py

--- PAI/Lab-02/Lab_Tasks/Q9.py
+++
@@ -1,8 +0,0 @@
-def build_message(**info):
-    key_and_value = []
-    for key, value in info.items():
-        key_and_value.append(f"{key}: {value}")
-    return ', '.join(key_and_value)
-
-message = build_message(name="Alice", age=30, city="New York", profession="Engineer")
-print(message)
