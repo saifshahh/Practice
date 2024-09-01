e266b6dc3ad94731d1ab2c3efd37966fb2b2aa1d
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-09-01 09:24:07 +0500

    Update Q2.cpp

--- DS/Lab-02/Q2.cpp
+++ DS/Lab-02/Q2.cpp
@@ -1,53 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int arr1[100][100];
-    int arr2[100][100];
-    int ans[100][100];
+int main(){
+    
+    int array1[10][10];
+    int array2[10][10];
+    int result[10][10];
     int size;
     int element;
 
-    cout << "Enter the size of first array: ";
-    cin >> size;
+    cout<<"Enter the size of the arrays: ";
+    cin>>size;
 
-    cout << "Input for array 1" << endl;
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            cout << "Enter the element " << j + 1 << ": ";
+    cout<<"Input for array 1: "<<endl;
+    for (int i=0; i<size; i++){
+        for(int j=0; j<size; j++){
+            cout<<"Enter the element ["<<i+1<<"]"<<"["<<j+1<<"]: ";
             cin >> element;
-            arr1[i][j] = element;
+            array1[i][j] = element;
         }
     }
 
-    cout << "Input for array 2" << endl;
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            cout << "Enter the element " << j + 1 << ": ";
+    cout<<"Input for array 2: "<<endl;
+    for (int i=0; i<size; i++){
+        for (int j=0; j<size; j++){
+            cout<<"Enter the element ["<<i+1<<"]"<<"["<<j+1<<"]: ";
             cin >> element;
-            arr2[i][j] = element;
+            array2[i][j] = element;
         }
     }
 
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            ans[i][j] = arr1[i][j] * arr2[i][j];
-        }
+    for (int i=0; i<size; i++){
+        for (int j=0; j<size; j++) result[i][j] = array1[i][j] * array2[i][j];
     }
 
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            cout << ans[i][j] << " ";
-        }
-        cout << endl;
+    for (int i=0; i<size; i++){
+        for(int j=0; j<size; j++) cout << result[i][j] << " ";
+        cout<<endl;
     }
 }
