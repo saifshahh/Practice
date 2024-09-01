8a7c188eafe579153f98ce2e1b56f4c96f3d317d
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-09-01 10:11:50 +0500

    Create T11.py

---
+++ PAI/Lab-02/Lab_Tasks/T11.py
@@ -0,0 +1,44 @@
+def add(Stud,grade,name):
+    Stud[name].append(grade) 
+    return Stud
+
+def avg(Stud,name):
+    if name in Stud:
+        total = Stud[name]
+        avg = sum(total)/len(total)
+        print(f"{name}'s average grade is {avg}.")
+    else:
+        print(f"Student {name} not found.")
+
+def add_stud(Stud, name):
+    if name not in Stud:
+        Stud[name] = []
+        print(f"Added new student {name} with an empty list of grades.")
+    else:
+        print(f"Student {name} already exists.")
+
+def remove(Stud,name):
+    if name in Stud:
+        del Stud[name]
+        return Stud
+    else:
+        print(f"Student {name} not found.")
+
+Stud= {}
+num = int(input('Enter the number of students: '))
+
+for i in range(num):
+    name = input('Enter the name: ')
+    grades = []
+    Num = int(input(f"Enter the number of grades for {name}: "))
+    
+    for j in range(Num):
+        grades.append(int(input(f"Enter grade {j + 1}: ")))
+    
+    Stud[name] = grades
+
+print(Stud)
+print(add(Stud,33,'Syed'))
+print(avg(Stud,'Saif'))
+print(add_stud(Stud,'Rehman'))
+print(remove(Stud,'Shah'))
