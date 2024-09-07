ef912d06b1f79270f89e7938a0d06f9881663b33
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-09-07 17:38:39 +0500

    Update Q1.py

--- PAI/Lab-03/Lab_Tasks/Q1.py
+++ PAI/Lab-03/Lab_Tasks/Q1.py
@@ -1,6 +1,9 @@
 characters = 0
-with open(r'E:\Programming\Lab-03\file.txt') as f:
-    for line in f:
-        characters += len(line.replace(' ','').replace('\t','').replace('\n',''))
-
+try:
+    with open(r'E:\Programming\Lab-03\file.txt') as f:
+        for line in f:
+            characters += len(line.replace(' ','').replace('\t','').replace('\n',''))
+except Exception as e:
+    print(str(e))
+    
 print(characters)
