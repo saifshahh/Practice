ff35c59d73cd6f3f730dd85840a0a813506e16fd
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-09-07 17:47:46 +0500

    Update Q2.py

--- PAI/Lab-03/Lab_Tasks/Q2.py
+++ PAI/Lab-03/Lab_Tasks/Q2.py
@@ -1,12 +1,10 @@
-with open('file.txt', 'r') as f:
-    # lines = f.readlines()
-    # for line in lines:
-    #     if line.find('l') != -1:
-    #         line.replace('l','h')
+try:
+    with open(r'E:\Programming\Lab-03\file.txt', 'r') as f:
+        text = f.read()
+        text = text.replace("l", "h")
+except Exception as e:
+    print(str(e))
 
-    text = f.read()
-    text = text.replace("l", "h")
-
-with open('file.txt', 'w') as f:
-        f.write(text)
-        print(text)
+with open(r'E:\Programming\Lab-03\file.txt', 'w') as f:
+    f.write(text)
+    print(text)
