e579d791414c3a18ebbc1ec70a1826a8b9e461a2
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-09-07 19:10:12 +0500

    Update Q6.py

--- PAI/Lab-03/Lab_Tasks/Q6.py
+++ PAI/Lab-03/Lab_Tasks/Q6.py
@@ -1,1 +1,14 @@
+s = str(input('Enter the sentence: '))
 
+try:
+    if s[len(s)-1] == '?':
+        with open('questions.txt', 'w') as f:
+            f.write(s)
+            print('It is a question.. writing it to the file...')
+    else:
+        print('Not a question..')
+        
+except IOError as e:
+    print(str(e))
+except Exception as a:
+    print(str(a))
