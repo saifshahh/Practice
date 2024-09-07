d9f4f04dd0f9ccaecdc067e174ec79bfd3e0d416
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-09-07 18:22:15 +0500

    Update Q3.py

--- PAI/Lab-03/Lab_Tasks/Q3.py
+++ PAI/Lab-03/Lab_Tasks/Q3.py
@@ -1,1 +1,21 @@
+num = int(input('Enter the length of lists: '))
+l1 = []
+l2 = []
+dict = {}
 
+print('Input for list 1: ')
+for i in range(num):
+    l1.append(int(input('Enter the number: ')))
+
+print('Input for list 2: ')
+for i in range(num):
+    l2.append(int(input('Enter the number: ')))
+
+for i in range(len(l1)):
+    dict[l1[i]] = l2[i]
+
+try:
+    with open(r'E:\Programming\Lab-03\file2.txt', 'w') as fileObj:
+       fileObj.write(str(dict))
+except Exception as e:
+    print(str(e))
