34ba338c70fb69c987e52ae9aafe4c0df19f3c7f
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-09-09 08:50:13 +0500

    Create selectionSort.cpp

---
+++ DS/Lab-04/selectionSort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int size){
+    for(int i=0; i<size-1; i++){   
+        int minIndex = i;
+        for(int j=i+1; j<size; j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex = j;
+            }
+        }
+        if(minIndex != i){
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
+
+void display(int arr[], int size){
+    for(int i=0; i<size; i++){
+        cout<<" "<<arr[i];
+    }
+}
+
+int main(){
+    int size = 5;
+    int arr[5] = {7,5,6,2,3};
+    selectionSort(arr, size);
+    display(arr,size);
+}
