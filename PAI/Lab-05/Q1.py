9d5b03b1ded7d0295a5a440657847dc50a128af5
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-09-18 08:47:00 +0500

    Update Q1.py

--- PAI/Lab-05/Q1.py
+++ PAI/Lab-05/Q1.py
@@ -1,1 +1,18 @@
-
+class Vehicle:
+    def __init__(self, seats):
+        self.seats = seats
+        
+    def calculate(self):
+        fare = self.seats*100
+        return fare;
+        
+class Bus(Vehicle):
+    def __init__(self, seats):
+        self.seats = seats
+        
+    def calculate(self):
+        fare = (self.seats*100) + (self.seats*100)*(10/100)
+        return fare
+    
+b = Bus(2)
+print("The total fare is: ",b.calculate())
