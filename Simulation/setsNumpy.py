4878d6b9c2d279725d14be1d8c5ed26b993d04a9
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-10-07 09:50:18 +0500

    Update setsNumpy.py

--- Simulation/setsNumpy.py
+++ Simulation/setsNumpy.py
@@ -1,6 +1,40 @@
+# import numpy as np
+
+# a = np.array([1,1,1,1,2,2,3,3,3,4,5,6,7])
+# x = np.unique(a)
+# print(x)
+
+# import numpy as np
+
+# a = np.array([1,3,4,6])
+# b = np.array([5,6,7])
+# x = np.union1d(a,b)
+# print(x)
+
+# import numpy as np
+
+# a = np.array([1,3,4,6])
+# b = np.array([5,6,7])
+# x = np.setdiff1d(a,b)
+# print(x)
+
+# import numpy as np
+
+# a = np.array([1,3,4,6])
+# b = np.array([5,6,7])
+# x = np.intersect1d(a,b)
+# print(x)
+
+# import numpy as np
+
+# a = np.array([1,3,4,6])
+# b = np.array([5,6,7])
+# x = np.setxor1d(a,b)
+# print(x)
+
 import numpy as np
 
-a = np.array([1,2,3,4,5,6,7])
-x = np.unique(a)
+a = np.array([1,3,4,6])
+b = np.array([5,6,7])
+x = np.isin(a,b)
 print(x)
-
