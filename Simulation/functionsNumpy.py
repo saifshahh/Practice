c2441e786adf5d7cab7ccf3804e3d9f1f261cb5c
Syed Saif ur Rehman Shah <144406935+iamunknowngamer@users.noreply.github.com>
2024-10-07 09:31:25 +0500

    Update functionsNumpy.py

--- Simulation/functionsNumpy.py
+++ Simulation/functionsNumpy.py
@@ -14,3 +14,37 @@ a = np.array([1,2,3,4,5,6,7,8])
 stir = np.frompyfunc(str, 1 ,1)
 
 print(stir(a))
+
+#################################################################################
+
+# import numpy as np
+
+# a = np.array([1,2,3,4])
+# b = np.array([1,2,3,4])
+
+# c = np.add(a,b)
+# print(c)
+
+# import numpy as np
+
+# a = np.array([1,2,3,4])
+# b = np.array([1,2,3,4])
+
+# c = np.subtract(a,b)
+# print(c)
+
+# import numpy as np
+
+# a = np.array([1,2,3,4])
+# b = np.array([1,2,3,4])
+
+# c = np.multiply(a,b)
+# print(c)
+
+import numpy as np
+
+a = np.array([1,2,3,4])
+b = np.array([1,2,3,4])
+
+c = np.divide(a,b)
+print(c)
